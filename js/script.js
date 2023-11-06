516c5b64cd543731d1db7013b016211fff97d723
Chrilleweb <christian.munknissen@gmail.com>
2023-11-06 18:56:29 +0100

    frontend styles components, footer, header and js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function fetchChatGPT() {
+    // Get user input values
+    const gender = document.getElementById("gender").value;
+    const nutritionType = document.getElementById("nutritionType").value;
+    const weight = parseFloat(document.getElementById("weight").value);
+    const height = parseFloat(document.getElementById("height").value);
+    const numberOfDays = parseInt(document.getElementById("numberOfDays").value);
+
+    // Prepare data for the request
+    const requestData = {
+        userInformation: {
+            gender: gender,
+            weight: weight,
+            height: height,
+        },
+        nutritionType: nutritionType,
+        numberOfDays: numberOfDays,
+    };
+
+    // Make a request to your ChatGPT service (replace with your API endpoint)
+    fetch('http://localhost:8081/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Display the response in the 'response' div
+            document.getElementById("response").innerText = JSON.stringify(data, null, 2);
+        })
+        .catch(error => console.error('Error:', error));
+}
